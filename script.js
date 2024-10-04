3a6ddfe29f9462ddd75400275cf4f868b976002b
Isadora Felix <isadora.felix.faria@escola.pr.gov.br>
2024-10-04 08:56:33 -0300

    aula1

---
+++ script.js
@@ -0,0 +1,13 @@
+//Como Eu Era Antes de Você romance/drama 12 anos
+//Harry Potter e o Prisioneiro de Azkaban infantil/fantasia livre
+//Miraculous: As Aventuras de Ladybug aventura livre
+//Avatar: A Lenda de Aang 
+//La casa de papel
+
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(200);
+  }
